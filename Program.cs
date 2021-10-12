3e58dc40a37d390786bcbb9f457f7a660de1e220
sbritobreno <breno.silvabrito'hotmail.com>
2021-10-12 14:55:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_lesson5_eightball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
